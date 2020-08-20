0797e9bdc9cef35af6ba734f0bfd3e0340419406
Maszuari <maszuari@yahoo.com>
2020-08-20 16:44:08 +0100

    Add functions to connect and save to DB.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/maszuari/csv2db
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 )
+
+type MyDb struct {
+	*sqlx.DB
+}
 
 func processCsv(filePath string) {
 
@@ -20,6 +26,8 @@ func processCsv(filePath string) {
 	defer f.Close()
 
 	r := csv.NewReader(f)
+	db := connectToDb()
+	myDb := MyDb{db}
 	for {
 
 		row, err := r.Read()
@@ -33,7 +41,26 @@ func processCsv(filePath string) {
 		}
 
 		fmt.Println(row[0])
+		myDb.saveRow(row[0])
 	}
+}
+
+func connectToDb() *sqlx.DB {
+	//TODO Set username, password and db name in .env
+	db, err := sqlx.Connect("mysql", "dbuser:dbpassword@(localhost:3306)/dbname")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return db
+}
+
+func (db *MyDb) saveRow(row string) {
+
+	tx := db.MustBegin()
+	db.MustExec("INSERT INTO test_one (code) VALUES (?)", row)
+	tx.Commit()
+
 }
 
 func main() {
